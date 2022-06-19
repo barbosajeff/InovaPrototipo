7de362a23728601f091636392bf2d5e40c31a1f2
barbosajeff <barbosa.jeff01@gmail.com>
2022-06-18 22:43:53 -0300

    kk

--- src/inova/App.java
+++ src/inova/App.java
@@ -15,6 +15,10 @@ public class App {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+        
+        System.out.println("inova.App.main()");
     }
     
 }
